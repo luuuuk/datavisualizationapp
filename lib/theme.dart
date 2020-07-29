3d3a2a938c69973cef027b45dfd3fc54565b7879
luuuuk <luk.schild@t-online.de>
2020-07-29 16:47:29 +0200

    Added theme file to hold information about the overall styling of the app

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class ThemeColors{
+  static const darkBlue = const Color(0xFF2D274C);
+  static const lightBlue = const Color(0xFF306FA5);
+  static const blueGreenis = const Color(0xFF95C4B9);
+  static const yellowGreenish = const Color(0xFFF6F2CB);
+  static const orange = const Color(0xFFF06A25);
+}
