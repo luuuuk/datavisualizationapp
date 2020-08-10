faa580bb973d2d261cc420720cd2d9b925053efd
luuuuk <luk.schild@t-online.de>
2020-08-10 10:14:40 +0200

    [Update] implemented ActivityGoal operations in DatabaseManager

--- lib/services/database_manager.dart
+++ lib/services/database_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:data_visualization_app/models/activity_goal.dart';
 import 'package:data_visualization_app/models/recorded_activity.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart';
@@ -102,5 +103,53 @@ class DatabaseManager {
       // Pass the Dog's id as a whereArg to prevent SQL injection.
       whereArgs: [activity.id],
     );
+  }
+
+  /// Save Goal
+  Future<int> saveGoal(ActivityGoal goal) async {
+    Database dbClient = await this.database;
+
+    var result = await dbClient.insert("goals", goal.toMap());
+    return result;
+  }
+
+  /// Update Goal
+  Future<void> updateGoal(ActivityGoal goal) async {
+    // Get a reference to the database.
+    Database dbClient = await this.database;
+
+    // Update the given Activity.
+    await dbClient.update(
+      'goals',
+      goal.toMap(),
+      // Ensure that the Dog has a matching id.
+      where: "id = ?",
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [goal.id],
+    );
+  }
+
+  /// Delete goal
+  Future<void> deleteGoal(ActivityGoal goal) async {
+    Database dbClient = await this.database;
+
+    var result = await dbClient.delete(
+      'goals',
+      where: "id = ?",
+      whereArgs: [goal.id],
+    );
+  }
+
+  /// Method to get all activities
+  Future<List<ActivityGoal>> getGoals() async {
+    var dbClient = await database;
+    List<Map> list = await dbClient.rawQuery('SELECT * FROM goals');
+    List<ActivityGoal> goals = new List();
+    for (int i = 0; i < list.length; i++) {
+      goals.add(
+          new ActivityGoal(list[i]["id"], list[i]["number"], list[i]["title"],
+              list[i]["type"], list[i]["activity"], list[i]["span"]));
+    }
+    return goals;
   }
 }
