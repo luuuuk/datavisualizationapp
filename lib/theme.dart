5915be7ef91d4b67caedf749f168b9cdaa8bb65b
luuuuk <luk.schild@t-online.de>
2020-08-15 13:08:23 +0200

    Added color shades for AddDataScreen

--- lib/theme.dart
+++ lib/theme.dart
@@ -7,6 +7,8 @@ class ThemeColors {
   static const darkBlue = const Color(0xFF2D274C);
   static const lightBlue = const Color(0xFF306FA5);
   static const blueGreenis = const Color(0xFF95C4B9);
+  static const blueGreenisShade1 = const Color(0xFF5E6A77);
+  static const blueGreenisShade2 = const Color(0xFF657288);
   static const yellowGreenish = const Color(0xFFF6F2CB);
   static const orange = const Color(0xFFF06A25);
 }
