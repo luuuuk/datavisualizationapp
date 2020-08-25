fddb82f4045c5f252204fc829fd2ace14a6a985f
luuuuk <luk.schild@t-online.de>
2020-08-25 22:21:16 +0200

    Changed AppIcon and set name to 'Banana Stats'

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Data Visualizer',
+      title: 'Banana Stats',
       debugShowCheckedModeBanner: false,
       theme: VisualizationTheme().theme,
       initialRoute: WelcomeScreen.routeName,
